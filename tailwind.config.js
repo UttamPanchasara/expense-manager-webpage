76fb4f8f391a6c11516393d51389c305dbdd79c3
Uttam <panchasarauttam@gmail.com>
2026-05-15 11:28:58 +0530

    - content update

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './privacy-policy.html',
+    './blog.html',
+    './blog/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
